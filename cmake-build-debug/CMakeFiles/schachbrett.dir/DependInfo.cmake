60fe9cac5eb56654455654f2d1f4aacc035573d8
Enthöfer Joshua <j.enthoefer@laudert.de>
2019-06-14 13:39:48 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/schachbrett.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jen/Desktop/FH/SS 19/Info2/CLionProjects FH/Poodle/8_Graphen/schachbrett/Dijkstratabelle.cpp" "/Users/jen/Desktop/FH/SS 19/Info2/CLionProjects FH/Poodle/8_Graphen/schachbrett/cmake-build-debug/CMakeFiles/schachbrett.dir/Dijkstratabelle.cpp.o"
+  "/Users/jen/Desktop/FH/SS 19/Info2/CLionProjects FH/Poodle/8_Graphen/schachbrett/Graph.cpp" "/Users/jen/Desktop/FH/SS 19/Info2/CLionProjects FH/Poodle/8_Graphen/schachbrett/cmake-build-debug/CMakeFiles/schachbrett.dir/Graph.cpp.o"
+  "/Users/jen/Desktop/FH/SS 19/Info2/CLionProjects FH/Poodle/8_Graphen/schachbrett/main.cpp" "/Users/jen/Desktop/FH/SS 19/Info2/CLionProjects FH/Poodle/8_Graphen/schachbrett/cmake-build-debug/CMakeFiles/schachbrett.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/schachbrett.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/schachbrett.dir/main.cpp.o"
+  "CMakeFiles/schachbrett.dir/Graph.cpp.o"
+  "CMakeFiles/schachbrett.dir/Dijkstratabelle.cpp.o"
+  "schachbrett.pdb"
+  "schachbrett"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/schachbrett.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
